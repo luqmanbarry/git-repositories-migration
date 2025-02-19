11ed2db0658c664a231714ef1125744e97f0fb1b
Luqman Barry <dhabbhoufaamou@gmail.com>
2025-02-19 11:23:47 -0600

    Update repo-migration.sh

--- repo-migration.sh
+++ repo-migration.sh
@@ -12,8 +12,8 @@ TARGET_REPOS_PROJECT_PAT="target-repos-pat"  # Replace with your target PAT
 TARGET_REPOS_PREFIX="team-name" # Common prefix all target repos have. Set to empty string if none
 
 CURRENT_DIR="$(pwd)"  # Current working directory
-WORKING_DIR="/${CURRENT_DIR}/repos_migration"  # Working directory for cloning repos
-REPORTS_DIR="/${CURRENT_DIR}/migration_reports"  # Directory where reports files will be stored
+WORKING_DIR="${CURRENT_DIR}/repo_migration"  # Working directory for cloning repos
+REPORTS_DIR="${CURRENT_DIR}/migration_reports/${TARGET_REPOS_PREFIX:- }"  # Directory where reports files will be stored
 PATCH_FILE="${WORKING_DIR}/source-state.patch"  # Patch file for merging source and target branches
 SUCCEEDED_REPORT_FILE="${REPORTS_DIR}/succeeded-migrations.csv"  # Succeeded code migrations report
 FAILED_REPORT_FILE="${REPORTS_DIR}/failed-migrations.csv"  # Failed code migrations report
@@ -29,28 +29,28 @@ mkdir -p "${REPORTS_DIR}"
 echo "source_repo_url, source_repo, source_branch, target_repo_url, target_repo" > "${SUCCEEDED_REPORT_FILE}"
 echo "source_repo_url, source_repo, source_branch, target_repo_url, target_repo" > "${FAILED_REPORT_FILE}"
 
-cd "${WORKING_DIR}"
 
-if [ ! -z "${REPOS_LIST_FILE}" -a -f "${REPOS_LIST_FILE}" -a -r "${REPOS_LIST_FILE}" ];
+if [  ! -z "${REPOS_LIST_FILE}" -a -f "${REPOS_LIST_FILE}" -a -r "${REPOS_LIST_FILE}" ];
 then
-  SOURCE_REPOS=$(cat $REPOS_LIST_FILE)
-  echo "=> Top N repos provided: "
-  echo $SOURCE_REPOS | head
-else
-  echo "=> Repos list file must be provided as input to the script."
-  echo "=> Example: ./repo-migration.sh source-repos.txt"
-  exit 1
-fi
-
-# Loop through each repository
-for REPO in $SOURCE_REPOS; do
+  # SOURCE_REPOS=$(<$REPOS_LIST_FILE)
+  while SOURCE_REPOS= read -r line; do
+    echo $line
+    REPO=$(echo "$line" | tr -d '\r' |  tr -d '\n')
     echo "==> Migrating repository: $REPO"
 
     SOURCE_REPO_URL="${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-    SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_PROJECT_PAT}@${SOURCE_REPOS_ORG_URL//https:///}/_git/${REPO}" "${REPO}"
-
+    SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
+    # SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_PROJECT_PAT}@${SOURCE_REPOS_ORG_URL}/_git/${REPO}" 
+    echo $SOURCE_REPO_URL_PAT
+  
+    
+    cd "${WORKING_DIR}"
     echo "==> Clone the source repository with all branches and tags..."
-    git clone --mirror "${SOURCE_REPO_URL_PAT}"
+    export GIT_PAT="$SOURCE_REPOS_PROJECT_PAT"
+    export GIT_TOKEN="$SOURCE_REPOS_PROJECT_PAT"
+     git clone  "${SOURCE_REPO_URL_PAT}" || true && echo "REPO exists already"
+
+    # git tfs clone ${SOURCE_REPOS_ORG_URL} "\$/$REPO" ./${REPO} --branches=all
     cd "${REPO}"
     echo "==> Set source remote url to repo..."
     git remote add source ${SOURCE_REPO_URL_PAT}
@@ -61,6 +61,8 @@ for REPO in $SOURCE_REPOS; do
 
     echo "==> Get a list of all branches in the source remote..."
     SOURCE_BRANCHES=$(git branch -r | grep "source/" | sed "s/source\///" | grep -v "HEAD")
+    echo $SOURCE_BRANCHES
+
 
     TARGET_REPO_URL=""
     TARGET_REPO_URL_PAT=""
@@ -69,44 +71,57 @@ for REPO in $SOURCE_REPOS; do
     then
       TARGET_REPO="${REPO}"
       TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-      TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL//https:///}/_git/${TARGET_REPO}"
+      TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+      # TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+      
     else
       TARGET_REPO="${TARGET_REPOS_PREFIX}-${REPO}"
       TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-      TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL//https:///}/_git/${TARGET_REPO}"
+      TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+      # TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+
     fi
     echo "==> Set target remote url to repo..."
     git remote add target ${TARGET_REPO_URL_PAT}
-
     # Copy individual branches from source to destination
-    for BRANCH in "${SOURCE_BRANCHES}"; do
-        echo "===> Copying branch ${BRANCH}..."
+    SOURCE_BRANCHES=($SOURCE_BRANCHES)
+    echo ${SOURCE_BRANCHES[@]}
 
+    for BRANCH in "${SOURCE_BRANCHES[@]}"; do
+        echo "===> Copying branch ${BRANCH}..."
+        BRANCH=$(echo $BRANCH | xargs)
         echo "===> Checkout the branch from the source remote"
-        git checkout -b ${BRANCH} "source/${BRANCH}"
+        git checkout -b "${BRANCH}" source/${BRANCH} || true
+       
+        git fetch 
+        git pull source ${BRANCH}
+
 
         echo "===> Save the source state to patch file"
         git diff HEAD > "${PATCH_FILE}"
 
-        echo "===> Reset to latest from target remote branch"
-        git fetch target ${BRANCH}
-        git reset --hard "target/${BRANCH}"
-
-        echo "===> Applying source state patch file..."
-        git apply "${PATCH_FILE}"
-
+        git push -u target ${BRANCH}
+        
         if [ "$?" == "0" ];
         then
           echo "===> Conflict resolution for branch '${BRANCH}' completed."
           echo "===> Push the branch to the target remote"
-          git commit -am "Repo Migration: Merged source and target branches."
-          git push target ${BRANCH}
+         
+          # git push target ${BRANCH}
 
           # Write succeeded report
           echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${SUCCEEDED_REPORT_FILE}"
         else
           echo "===> Conflict resolution for branch '${BRANCH}' failed. Check the ${FAILED_REPORT_FILE} file"
+          echo "===> Reset to latest from target remote branch"
+          git fetch target ${BRANCH}
+          git reset --hard "target/${BRANCH}"
 
+          echo "===> Applying source state patch file..."
+          git apply "${PATCH_FILE}"
+          git commit -am "Repo Migration: Merged source and target branches."
+        
+           git push target ${BRANCH}
           # Write failed report
           echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${FAILED_REPORT_FILE}"
         fi
@@ -115,13 +130,13 @@ for REPO in $SOURCE_REPOS; do
     echo "==> Copying tags from source to target..."
     git push target --tags
 
-    # Clean up
-    cd ..
-    rm -rf "${REPO}"
-done
+  done < $REPOS_LIST_FILE
+ 
+else
+  echo "=> Repos list file must be provided as input to the script."
+  echo "=> Example: ./repo-migration.sh source-repos.txt"
+  exit 1
+fi
 
-# Clean up working directory
-cd ..
-rm -rf "$WORKING_DIR"
 echo "=> Migration completed!"
 echo "=> Check the reports files in: ${REPORTS_DIR}"
