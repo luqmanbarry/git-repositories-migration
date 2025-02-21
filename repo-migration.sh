9d1f81bcd4a0467e961ff7fad25c16eb5b25f2b7
Luqman Barry <dhabbhoufaamou@gmail.com>
2025-02-21 10:16:54 -0600

    Update repo-migration.sh

--- repo-migration.sh
+++ repo-migration.sh
@@ -3,15 +3,16 @@ set +e
 ## ALL GIT REPOS URLS INCLUDE "_git", THIS IS AZURE SPECIFIC
 ## REMOVE THE SUBSTRING (_git) IF IT DOES NOT APPLY TO YOU
 
-# Configuration
+# Configuration - Set these variables before running the script
 # LEAVE OUT https://
 SOURCE_REPOS_ORG_URL="source.example.com/source-org/source-project"  # Replace with your source Azure Repos URL.
 # LEAVE OUT https://
 TARGET_REPOS_ORG_URL="target.example.com/target-org/target-project"  # Replace with your target Azure DevOps URL.
-SOURCE_REPOS_PROJECT_PAT="source-repos-pat"  # Replace with your source PAT
-TARGET_REPOS_PROJECT_PAT="target-repos-pat"  # Replace with your target PAT
-TARGET_REPOS_PREFIX="team-name" # Common prefix all target repos have. Set to empty string if none
-CLEANUP_BINARY_FILES=true # Set this flag to true if you want large files removed from git history
+SOURCE_REPOS_PROJECT_PAT=""  # Replace with your source PAT. Leave empty if you've already setup Git Credentials Helper for this url
+TARGET_REPOS_PROJECT_PAT=""  # Replace with your target PAT. Leave empty if you've already setup Git Credentials Helper for this url
+TARGET_REPOS_PREFIX="team-name"  # Common prefix all target repos have. Set to empty string if none
+CLEANUP_LARGE_FILES=true  # Set this flag to true if you want large files removed from git history
+LARGE_FILE_SIZE="5M"  # Potential values: 500K, 1M, 2M, 3M, 10M,..
 
 
 CURRENT_DIR="$(pwd)"  # Current working directory
@@ -44,8 +45,12 @@ then
       echo "==> Migrating repository: $REPO"
 
       SOURCE_REPO_URL="${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-      # SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-      SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_PROJECT_PAT}@${SOURCE_REPOS_ORG_URL}/_git/${REPO}" 
+      if [ -z "${SOURCE_REPOS_PROJECT_PAT}" ];
+      then
+        SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
+      else
+        SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_PROJECT_PAT}@${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
+      fi 
       echo $SOURCE_REPO_URL_PAT
       
       cd "${WORKING_DIR}"
@@ -75,14 +80,25 @@ then
       then
         TARGET_REPO="${REPO}"
         TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        # TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+
+        if [ -z "${TARGET_REPOS_PROJECT_PAT}" ];
+        then
+          TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+        else
+          TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+        fi
         
       else
         TARGET_REPO="${TARGET_REPOS_PREFIX}-${REPO}"
         TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        # TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+
+        if [ -z "${TARGET_REPOS_PROJECT_PAT}" ];
+        then
+          TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+        else
+          TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+        fi
+
 
       fi
       echo "==> Set target remote url to repo..."
@@ -112,7 +128,7 @@ then
             # Write succeeded report
             echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${SUCCEEDED_REPORT_FILE}"
           else
-            if [ "$CLEANUP_BINARY_FILES" == "true" ];
+            if [ "$CLEANUP_LARGE_FILES" == "true" ];
             then
               echo "===> Cleaning up binary files form the git log..."
 
@@ -159,7 +175,9 @@ then
                 --invert-paths --force
 
               echo "====> Remove large files from history. Example: 1M, 5M, 10M"
-              git filter-repo --strip-blobs-bigger-than 5M --force
+              git filter-repo \
+                --strip-blobs-bigger-than $LARGE_FILE_SIZE \
+                --force
 
               echo "====> Clean up the repository"
               git gc --aggressive --prune=now
