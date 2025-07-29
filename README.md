d787b4921a668742e06944048dfc8d43ca85afd1
Luqman Barry <lbarry@redhat.com>
2025-07-29 10:48:42 -0500

    Improved scripts

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
 # Git Repositories Migration
 
-While preserving git history, this bash script migrates repositories including branches, tags, commit log, from one git provider to another. 
+While preserving git history, this bash script migrates repositories including branches, tags, commit log, from one git organization to another. The script support code migration between git providers as well; for example: BitBucket to GitHub, Self hosted git server to Azure DevOps.
 
 The script has logic to clean up files with certain extensions ([see script](./repo-migration.sh)); and files with sizes reaching a defined value, the default is 5 Megabytes (5M).
 
 To enable Large File cleanup, set these two parameters (enabled by default):
-- `CLEANUP_LARGE_FILES=true`: Set to true to enable the file cleanup, false for otherwise.
-- `LARGE_FILE_SIZE="5M"`: Set the large file size limit using this variable. The default is 5M.
+
+- `large_file_cleanup.enable: true`: Set to true to enable the file cleanup, false for otherwise.
+- `large_file_cleanup.file_size: 5M`: Set the large file size limit using this variable. The default is 5M.
 - Files extensions are defined within the script, you can add or remove extensions per your needs.
 
 ## Pre-requisites
+
+- Install Python >= 3.12.x (If running the Python script)
 - Install Git
-   - Instructions: `https://git-scm.com/book/en/v2/Getting-Started-Installing-Git`   
+   - Instructions: `https://github.com/git-guides/install-git`
+- Install yq
+  - Instructions: `https://github.com/mikefarah/yq?tab=readme-ov-file#install`
 - Install git-filter-repo package
    - Instructions: `https://github.com/newren/git-filter-repo/blob/main/INSTALL.md`
 - Configure the Git credentials helper: `https://git-scm.com/docs/gitcredentials`
 - Script works best on a native Linux distribution
    - If on Windows, the script does not work well with Git Bash; instead set up Windows Subsystem for Linux (WSL)
       - Configure Git: `https://learn.microsoft.com/en-us/windows/wsl/tutorials/wsl-git`
-- Git config file setup: `https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup`
-- Generate project-scoped PAT tokens for source and target repos
+- Configure git authentication for the source and target organizations; if configured properly, you should be able to clone from both git organizations without being prompted for credentials
 
 ## Procedure
 
@@ -27,48 +31,40 @@ To enable Large File cleanup, set these two parameters (enabled by default):
    ```sh
    chmod 755 repo-migration.sh
    ```
-2. Install Pyathon >= 3.12.x (If running the Python script)
 
-3. Provide the list of source repositories in a file called repos.txt (add empty line at the end)
-   ```sh
-   cat > repos.txt << EOF
-    repo1
-    repo2
-    repo3
-   EOF
-   ```
-3. Set the variables in the repo-migration.sh or repo_migration.py file
-   ```sh
-   # Configuration - Set these variables before running the script
-   # LEAVE OUT https://
-   SOURCE_REPOS_ORG_URL="source.example.com/source-org/source-project"  # Replace with your source Azure Repos URL.
-   # LEAVE OUT https://
-   TARGET_REPOS_ORG_URL="target.example.com/target-org/target-project"  # Replace with your target Azure DevOps URL.
-   SOURCE_REPOS_PROJECT_PAT=""  # Replace with your source PAT. Leave empty if you've already setup Git Credentials Helper for this url
-   TARGET_REPOS_PROJECT_PAT=""  # Replace with your target PAT. Leave empty if you've already setup Git Credentials Helper for this url
-   TARGET_REPOS_PREFIX="team-name"  # Common prefix all target repos have. Set to empty string if none
-   CLEANUP_LARGE_FILES=true  # Set this flag to true if you want large files removed from git history
-   LARGE_FILE_SIZE="5M"  # Potential values: 500K, 1M, 2M, 3M, 10M,..
+2. Populate the [inputs.yaml](./inputs.yaml) file with your repositories information.
+   
+   ```yaml
+   # Configuration - Set these variables in the inputs.yaml file before running the script
+   inputs:
+      source_project_url: https://github.com/project1
+      destination_project_url: https://github.com/project2
+      large_file_cleanup:
+         enable: true # Set false disable
+         file_size: 5M # Could be 1M, 2M, 3M, etc
+      repositories:
+         - source: my-src-repository1
+            destination: my-dest-repository1
+         - source: my-src-repository2
+            destination: my-dest-repository2
+         - source: my-src-repository3
+            destination: my-dest-repository3
+      
    ```
 
-4. Execute the script
+3. Execute the script
 
-   Bash:
    ```sh
-   ./repo-migration.sh repos.txt 2>&1 | tee "repo-migrations-bash-$(date +%Y%m%d%H%M).log"
-   ```
-   Python:
-   ```sh
-   repo_migration.py repos.txt 2>&1 | tee "repo-migrations-python-$(date +%Y%m%d%H%M).log"
+   ./repo-migration.sh 2>&1 | tee "repo-migrations-bash-$(date +%Y%m%d%H%M).log"
    ```
 
-## Git Large File Size Cleanup
+# Git large file size cleanup for a single repository
 
 1. Run the bash script
    
    ```sh
    ./git-large-file-size-fix.sh <path_to_git_directory>
-   # Example: If I am already in the git directory: ./git-large-file-size-fix.sh ./
+   # Example: If I am already in the git directory: ./git-large-file-size-fix.sh .
    ```
 
 2. Commit and Push
--- repo-migration.sh
+++ repo-migration.sh
@@ -1,29 +1,36 @@
 #!/bin/bash
 set +e
 set -x
-## ALL GIT REPOS URLS INCLUDE "_git", THIS IS AZURE SPECIFIC
-## REMOVE THE SUBSTRING (_git) IF IT DOES NOT APPLY TO YOU
 
-# Configuration - Set these variables before running the script
-# LEAVE OUT https://
-SOURCE_REPOS_ORG_URL="source.example.com/source-org/source-project"  # Replace with your source Azure Repos URL.
-# LEAVE OUT https://
-TARGET_REPOS_ORG_URL="target.example.com/target-org/target-project"  # Replace with your target Azure DevOps URL.
-SOURCE_REPOS_PROJECT_PAT=""  # Replace with your source PAT. Leave empty if you've already setup Git Credentials Helper for this url
-TARGET_REPOS_PROJECT_PAT=""  # Replace with your target PAT. Leave empty if you've already setup Git Credentials Helper for this url
-TARGET_REPOS_PREFIX="team-name"  # Common prefix all target repos have. Set to empty string if none
-CLEANUP_LARGE_FILES=true  # Set this flag to true if you want large files removed from git history
-LARGE_FILE_SIZE="5M"  # Potential values: 500K, 1M, 2M, 3M, 10M,..
+# Place the inputs.yaml file in the same directory as the bash script.
+
+# Configuration - Set these variables in the inputs.yaml file before running the script
+INPUT_YAML_FILE="inputs.yaml"
+if [ -f "$INPUT_YAML_FILE" ]; then
+  echo "File '$INPUT_YAML_FILE' exists and is a regular file."
+else
+  echo "File '$INPUT_YAML_FILE' does not exist or is not a regular file."
+fi
+
+if [ ! -s "$INPUT_YAML_FILE" ]; then
+  echo "The file '$INPUT_YAML_FILE' is empty or does not exist."
+else
+  echo "The file '$INPUT_YAML_FILE' exists and is not empty."
+fi
+
+SOURCE_REPOS_ORG_URL="$(yq e '.inputs.source_project_url' $INPUT_YAML_FILE)"
+TARGET_REPOS_ORG_URL="$(yq e '.inputs.destination_project_url' $INPUT_YAML_FILE)"
+CLEANUP_LARGE_FILES="$(yq e '.inputs.large_file_cleanup.enable' $INPUT_YAML_FILE)"
+LARGE_FILE_SIZE="$(yq e '.inputs.large_file_cleanup.file_size' $INPUT_YAML_FILE)"
 
 
 CURRENT_DIR="$(pwd)"  # Current working directory
-WORKING_DIR="${CURRENT_DIR}/repo_migration_$(date +%Y%m%d%H)"  # Working directory for cloning repos
+WORKING_DIR="${CURRENT_DIR}/repo_migration/runs_$(date +%Y%m%d%H)"  # Working directory for cloning repos
 REPORTS_DIR="${CURRENT_DIR}/migration_reports/${TARGET_REPOS_PREFIX:- }"  # Directory where reports files will be stored
 PATCH_FILE="${WORKING_DIR}/source-state.patch"  # Patch file for merging source and target branches
 SUCCEEDED_REPORT_FILE="${REPORTS_DIR}/succeeded-migrations.csv"  # Succeeded code migrations report
 FAILED_REPORT_FILE="${REPORTS_DIR}/failed-migrations.csv"  # Failed code migrations report
-
-REPOS_LIST_FILE="$1"  # Path to file containing list of source repos to mirror (one repo per line)
+REPOS_LIST="$(yq '.inputs.repositories' )"
 SOURCE_REPOS=""
 
 # Create directories tree and files
@@ -35,199 +42,174 @@ echo "source_repo_url, source_repo, source_branch, target_repo_url, target_repo"
 echo "source_repo_url, source_repo, source_branch, target_repo_url, target_repo" > "${FAILED_REPORT_FILE}"
 
 
-if [  ! -z "${REPOS_LIST_FILE}" -a -f "${REPOS_LIST_FILE}" -a -r "${REPOS_LIST_FILE}" ];
+if [  ! -z "${REPOS_LIST}" ];
 then
   # SOURCE_REPOS=$(<$REPOS_LIST_FILE)
-  while SOURCE_REPOS= read -r line; do
-    echo $line
-    REPO=$(echo "$line" | tr -d '\r' |  tr -d '\n' | xargs)
-    if [ ! -z "$REPO" ];
+  for REPO in $REPOS_LIST;
+  do
+    SRC_REPO_NAME=$(echo $REPO | yq .source | tr -d '\r' |  tr -d '\n' | xargs)
+    DEST_REPO_NAME=$(echo $REPO | yq .destination | tr -d '\r' |  tr -d '\n' | xargs)
+
+    if [ -z "$SRC_REPO_NAME" ] || [ -z "$DEST_REPO_NAME" ];
     then
-      echo "==> Migrating repository: $REPO"
+      echo "One of Source ("$SRC_REPO_NAME") and/or destination ("$DEST_REPO_NAME") repository entries is empty."
+      exit 1
+    fi
 
-      SOURCE_REPO_URL="${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-      if [ -z "${SOURCE_REPOS_PROJECT_PAT}" ];
-      then
-        SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-      else
-        SOURCE_REPO_URL_PAT="https://${SOURCE_REPOS_PROJECT_PAT}@${SOURCE_REPOS_ORG_URL}/_git/${REPO}"
-      fi 
-      echo $SOURCE_REPO_URL_PAT
+    SOURCE_REPO_URL="${SOURCE_REPOS_ORG_URL}/${SRC_REPO_NAME}"
+    if [[ "$SOURCE_REPOS_ORG_URL" == *"dev.azure.com"* ]]; then
+      SOURCE_REPO_URL="${SOURCE_REPOS_ORG_URL}/_git/${SRC_REPO_NAME}"
+    fi
+
+    TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/${DEST_REPO_NAME}"
+    if [[ "$TARGET_REPOS_ORG_URL" == *"dev.azure.com"* ]]; then
+      TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${DEST_REPO_NAME}"
+    fi
+
+    echo "==> Migrating repository: $SOURCE_REPO_URL"
+    
+    cd "${WORKING_DIR}"
+    echo "==> Clone the source repository with all branches and tags..."
+    git clone "${SOURCE_REPO_URL}" || true && echo "REPO exists already"
+
+    cd "${SRC_REPO_NAME}"
+    echo "==> Set source remote url to repo..."
+    git remote add source ${SOURCE_REPO_URL}
+
+    # Fetch all branches and tags from the source remote
+    echo "==> Fetching all branches and tags from ${SOURCE_REPO_URL}..."
+    git fetch source --tags
+
+    echo "==> Get a list of all branches in the source remote..."
+    SOURCE_BRANCHES=$(git branch -r | grep "source/" | sed "s/source\///" | grep -v "HEAD")
+    echo $SOURCE_BRANCHES
+
+
+    echo "==> Set target remote url to repo..."
+    git remote add target ${TARGET_REPO_URL}
+    # Copy individual branches from source to destination
+    SOURCE_BRANCHES=($SOURCE_BRANCHES)
+    echo ${SOURCE_BRANCHES[@]}
+
+    for BRANCH in "${SOURCE_BRANCHES[@]}"; do
+        echo "===> Copying branch ${BRANCH}..."
+        BRANCH=$(echo $BRANCH | xargs)
+        echo "===> Checkout the branch from the source remote"
+        git checkout -b "${BRANCH}" source/${BRANCH} || true
       
-      cd "${WORKING_DIR}"
-      echo "==> Clone the source repository with all branches and tags..."
-      export GIT_PAT="$SOURCE_REPOS_PROJECT_PAT"
-      export GIT_TOKEN="$SOURCE_REPOS_PROJECT_PAT"
-      git clone  "${SOURCE_REPO_URL_PAT}" || true && echo "REPO exists already"
-
-      # git tfs clone ${SOURCE_REPOS_ORG_URL} "\$/$REPO" ./${REPO} --branches=all
-      cd "${REPO}"
-      echo "==> Set source remote url to repo..."
-      git remote add source ${SOURCE_REPO_URL_PAT}
-
-      # Fetch all branches and tags from the source remote
-      echo "==> Fetching all branches and tags from ${SOURCE_REPO_URL}..."
-      git fetch source --tags
-
-      echo "==> Get a list of all branches in the source remote..."
-      SOURCE_BRANCHES=$(git branch -r | grep "source/" | sed "s/source\///" | grep -v "HEAD")
-      echo $SOURCE_BRANCHES
+        git fetch 
+        git pull source ${BRANCH}
 
 
-      TARGET_REPO_URL=""
-      TARGET_REPO_URL_PAT=""
+        echo "===> Save the source state to a patch file"
+        git diff HEAD > "${PATCH_FILE}"
 
-      if [ -z "${TARGET_REPOS_PREFIX}" ];
-      then
-        TARGET_REPO="${REPO}"
-        TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-
-        if [ -z "${TARGET_REPOS_PROJECT_PAT}" ];
-        then
-          TARGET_REPO_URL_PAT="https://${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        else
-          TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        fi
+        git push -u target ${BRANCH}
         
-      else
-        TARGET_REPO="${TARGET_REPOS_PREFIX}-${REPO}"
-        TARGET_REPO_URL="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-
-        if [ -z "${TARGET_REPOS_PROJECT_PAT}" ];
+        if [ "$?" == "0" ];
         then
-          TARGET_REPO_URL_PAT="${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
+          echo "===> No resolutions for branch '${BRANCH}'."
+          # Write succeeded report
+          echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${SUCCEEDED_REPORT_FILE}"
         else
-          TARGET_REPO_URL_PAT="https://${TARGET_REPOS_PROJECT_PAT}@${TARGET_REPOS_ORG_URL}/_git/${TARGET_REPO}"
-        fi
-
-
-      fi
-      echo "==> Set target remote url to repo..."
-      git remote add target ${TARGET_REPO_URL_PAT}
-      # Copy individual branches from source to destination
-      SOURCE_BRANCHES=($SOURCE_BRANCHES)
-      echo ${SOURCE_BRANCHES[@]}
-
-      for BRANCH in "${SOURCE_BRANCHES[@]}"; do
-          echo "===> Copying branch ${BRANCH}..."
-          BRANCH=$(echo $BRANCH | xargs)
-          echo "===> Checkout the branch from the source remote"
-          git checkout -b "${BRANCH}" source/${BRANCH} || true
-        
-          git fetch 
-          git pull source ${BRANCH}
-
-
-          echo "===> Save the source state to patch file"
-          git diff HEAD > "${PATCH_FILE}"
-
-          git push -u target ${BRANCH}
-          
-          if [ "$?" == "0" ];
+          if [ "$CLEANUP_LARGE_FILES" == "true" ];
           then
+          
+            if git filter-repo --version &> /dev/null; then
+              echo "==> git filter-repo is installed and working."
+            else
+              echo "==> git filter-repo is not installed or not working."
+              echo "==> Find install instructions at this url: https://github.com/newren/git-filter-repo/blob/main/INSTALL.md"
+              exit 1
+            fi
+            
+            echo "===> Cleaning up binary files from the git log..."
+            echo "~~~> Repository size BEFORE cleanup: $(du -sh .)"
+
+            echo "====> Repack the repository"
+            git repack -a -d --depth=300 --window=300
+
+            echo "====> Remove files with binary extensions from git history"
+            git filter-repo \
+              --path-glob '*.zip' \
+              --path-glob '*.xls' \
+              --path-glob '*.tar' \
+              --path-glob '*.jar' \
+              --path-glob '*.gz' \
+              --path-glob '*.mov' \
+              --path-glob '*.avi' \
+              --path-glob '*.iso' \
+              --path-glob '*.msi' \
+              --path-glob '*.mp4' \
+              --path-glob '*.war' \
+              --path-glob '*.exe' \
+              --path-glob '*.dll' \
+              --path-glob '*.deb' \
+              --path-glob '*.vob' \
+              --path-glob '*.odt' \
+              --path-glob '*.docx' \
+              --path-glob '*.doc' \
+              --path-glob '*.tgz' \
+              --path-glob '*.rar' \
+              --path-glob '*.bz2' \
+              --path-glob '*.bzip2' \
+              --path-glob '*.7z' \
+              --path-glob '*.pptx' \
+              --path-glob '*.xlsm' \
+              --path-glob '*.xlsb' \
+              --path-glob '*.xltx' \
+              --path-glob '*.xlsx' \
+              --path-glob '*.pkg' \
+              --path-glob '*.rpm' \
+              --path-glob '*.tar.gz' \
+              --path-glob '*.dmg' \
+              --path-glob '*.bin' \
+              --path-glob 'node_modules/**' \
+              --path-glob '**/node_modules/**' \
+              --invert-paths \
+              --force
+
+            echo "====> Remove large files from history. Example: 1M, 5M, 10M"
+            git filter-repo \
+              --strip-blobs-bigger-than $LARGE_FILE_SIZE \
+              --invert-paths \
+              --force
+
+            echo "====> Clean up the repository"
+            git gc --aggressive --prune=now
+
+            echo "====> Verify the repository .git log size"
+            du -sh .git
+            echo "~~~> Repository size AFTER cleanup: $(du -sh .)"
+            git commit -am "Repo Migration: Removed binary files."
+            git push target ${BRANCH}
+          fi
+          echo "===> Conflict resolution for branch '${BRANCH}' failed. Check the ${FAILED_REPORT_FILE} file"
+          echo "===> Reset to latest from target remote branch"
+          git fetch target ${BRANCH}
+          git reset --hard "target/${BRANCH}"
+
+          echo "===> Applying source state patch file..."
+          git apply "${PATCH_FILE}"
+          git commit -am "Repo Migration: Merged source and target branches."
+        
+          git push target ${BRANCH}
+          if [ "$?" != "0" ];
+          then
+            # Write failed report
+            echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${FAILED_REPORT_FILE}"
+          else
             echo "===> No resolutions for branch '${BRANCH}'."
             # Write succeeded report
             echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${SUCCEEDED_REPORT_FILE}"
-          else
-            if [ "$CLEANUP_LARGE_FILES" == "true" ];
-            then
-            
-              if git filter-repo --version &> /dev/null; then
-                echo "==> git filter-repo is installed and working."
-              else
-                echo "==> git filter-repo is not installed or not working."
-                echo "==> Find install instructions at this url: https://github.com/newren/git-filter-repo/blob/main/INSTALL.md"
-                exit 1
-              fi
-              
-              echo "===> Cleaning up binary files from the git log..."
-              echo "~~~> Repository size BEFORE cleanup: $(du -sh .)"
-
-              echo "====> Repack the repository"
-              git repack -a -d --depth=300 --window=300
-
-              echo "====> Remove files with binary extensions from git history"
-              git filter-repo \
-                --path-glob '*.zip' \
-                --path-glob '*.xls' \
-                --path-glob '*.tar' \
-                --path-glob '*.jar' \
-                --path-glob '*.gz' \
-                --path-glob '*.mov' \
-                --path-glob '*.avi' \
-                --path-glob '*.iso' \
-                --path-glob '*.msi' \
-                --path-glob '*.mp4' \
-                --path-glob '*.war' \
-                --path-glob '*.exe' \
-                --path-glob '*.dll' \
-                --path-glob '*.deb' \
-                --path-glob '*.vob' \
-                --path-glob '*.odt' \
-                --path-glob '*.docx' \
-                --path-glob '*.doc' \
-                --path-glob '*.tgz' \
-                --path-glob '*.rar' \
-                --path-glob '*.bz2' \
-                --path-glob '*.bzip2' \
-                --path-glob '*.7z' \
-                --path-glob '*.pptx' \
-                --path-glob '*.xlsm' \
-                --path-glob '*.xlsb' \
-                --path-glob '*.xltx' \
-                --path-glob '*.xlsx' \
-                --path-glob '*.pkg' \
-                --path-glob '*.rpm' \
-                --path-glob '*.tar.gz' \
-                --path-glob '*.dmg' \
-                --path-glob '*.bin' \
-                --path-glob 'node_modules/**' \
-                --path-glob '**/node_modules/**' \
-                --invert-paths \
-                --force
-
-              echo "====> Remove large files from history. Example: 1M, 5M, 10M"
-              git filter-repo \
-                --strip-blobs-bigger-than $LARGE_FILE_SIZE \
-                --invert-paths \
-                --force
-
-              echo "====> Clean up the repository"
-              git gc --aggressive --prune=now
-
-              echo "====> Verify the repository size"
-              du -sh .git
-              echo "~~~> Repository size AFTER cleanup: $(du -sh .)"
-              git commit -am "Repo Migration: Removed binary files."
-              git push target ${BRANCH}
-            fi
-            echo "===> Conflict resolution for branch '${BRANCH}' failed. Check the ${FAILED_REPORT_FILE} file"
-            echo "===> Reset to latest from target remote branch"
-            git fetch target ${BRANCH}
-            git reset --hard "target/${BRANCH}"
-
-            echo "===> Applying source state patch file..."
-            git apply "${PATCH_FILE}"
-            git commit -am "Repo Migration: Merged source and target branches."
-          
-            git push target ${BRANCH}
-            if [ "$?" != "0" ];
-            then
-              # Write failed report
-              echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${FAILED_REPORT_FILE}"
-            else
-              echo "===> No resolutions for branch '${BRANCH}'."
-              # Write succeeded report
-              echo "${SOURCE_REPO_URL}, ${REPO}, ${BRANCH}, ${TARGET_REPO_URL}, ${TARGET_REPO}" >> "${SUCCEEDED_REPORT_FILE}"
-            fi
           fi
-      done
+        fi
+    done
 
-      echo "==> Copying tags from source to target..."
-      git push target --tags
-    fi
+    echo "==> Copying tags from source to target..."
+    git push target --tags
 
-  done < $REPOS_LIST_FILE
+  done
  
 else
   echo "=> Repos list file must be provided as input to the script."
